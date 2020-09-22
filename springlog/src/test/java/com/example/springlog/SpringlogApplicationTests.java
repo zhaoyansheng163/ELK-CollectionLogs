c3f217596eb5cb099c2bce85a9336a7b6b96d91f
jansenzhao <jansenzhao@foxmail.com>
2020-09-22 14:12:38 +0800

    add maven version

---
+++ springlog/src/test/java/com/example/springlog/SpringlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
